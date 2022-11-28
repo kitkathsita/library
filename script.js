373d24b979d4391cb46cfff05e1c9820fe8d36c3
kitkath <katherinpgr_99@hotmail.com>
2022-11-27 21:42:10 -0500

    the card show the element information but the delte button still not working

--- script.js
+++ script.js
@@ -2,32 +2,38 @@ const addButton = document.getElementById('addBookButton')
 const titleInfo = document.getElementById('title')
 const authorInfo = document.getElementById('autor')
 const pagesInfo = document.getElementById('pages')
+const genreInfo = document.getElementById('genre')
 const inputs = document.querySelectorAll('input')
 const formInfo = document.querySelector('.overlay')
 const content = document.querySelector('main')
 const buttonBook = document.querySelector('.addButton')
+let deleteButton = document.querySelectorAll('.deleteBut')
 
 let myLibrary = []
 
-function Book(title, author, pages) {
+function Book(title, author, pages, genre) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.genre = genre
 }
 
 function addBookToLibrary() {
   let titleBook = titleInfo.value
   let authorBook = authorInfo.value
   let pagesBook = pagesInfo.value
-  let newBook = new Book(titleBook, authorBook, pagesBook)
+  let genreBook = genreInfo.value
+  let newBook = new Book(titleBook, authorBook, pagesBook, genreBook)
   myLibrary.push(newBook)
-  //eraseInfo()
-  // closeForm()
+  eraseInfo()
+  closeForm()
   createCard()
+  deleteButton = document.querySelectorAll('.deleteBut')
+  deleteButton.forEach(deleteBut => deleteBut.addEventListener('click', deleteElement))
 }
 
 function eraseInfo() {
-  inputs.forEach().value = ''
+  inputs.forEach(inputs => inputs.value = '')
 }
 
 function closeForm() {
@@ -35,18 +41,18 @@ function closeForm() {
 }
 
 function createCard() {
+  let bookNumber = myLibrary.length
   let newCard = document.createElement('div')
-  content.appendChild(newCard)
   content.insertBefore(newCard, buttonBook)
   newCard.classList.add('card')
 
   newCard.innerHTML = 
   `<div class="titleContent">
-    <h2>Título</h2>
+    <div class="titulo">${myLibrary[bookNumber-1].title}</div>
     <div>
-      <p>autor:</p>
-      <p>páginas:</p>
-      <p>género:</p>
+      <p>autor: ${myLibrary[bookNumber-1].author}</p>
+      <p>páginas: ${myLibrary[bookNumber-1].pages}</p>
+      <p>género: ${myLibrary[bookNumber-1].genre}</p>
     </div>
   </div>
   <div class="options">
@@ -57,8 +63,21 @@ function createCard() {
         <span class="slider round"></span>
       </label>
     </div>
-    <i class="fa-regular fa-trash-can"></i>
+    <i class="fa-regular fa-trash-can deleteBut"></i>
   </div>`
+
+}
+
+function deleteElement() {
+  let parentDelete = this.parentNode.previousElementSibling.firstChild.nextElementSibling
+  console.log(parentDelete.innerHTML)
+  let indice = myLibrary.findIndex(bookElem => bookElem.title === parentDelete.innerHTML)
+  console.log(indice)
+}
+
+function print() {
+  console.log('hola')
 }
 
 addButton.addEventListener('click', addBookToLibrary)
+
